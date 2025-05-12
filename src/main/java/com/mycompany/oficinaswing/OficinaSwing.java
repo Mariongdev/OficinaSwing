525317c9e619f5e39d879860d142d443c7f4f3a6
Mariongdev <163408521+Mariongdev@users.noreply.github.com>
2025-05-12 17:58:04 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/oficinaswing/OficinaSwing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oficinaswing;
+
+/**
+ *
+ * @author PC 02
+ */
+public class OficinaSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
